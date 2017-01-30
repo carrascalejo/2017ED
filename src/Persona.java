2616515c27de1ffcfe54b75075b8ee21bba177cc
david <david@david.com>
2017-01-30 16:57:25 +0100

    Primera versión

---
+++ src/Persona.java
@@ -0,0 +1,45 @@
+
+public class Persona {
+	
+private	byte edad=0;
+private String nombre="";
+private double peso=0;
+/**
+ * @return the edad
+ */
+public byte getEdad() {
+	return edad;
+}
+/**
+ * @param edad the edad to set
+ */
+public void setEdad(byte edad) {
+	this.edad = edad;
+}
+/**
+ * @return the nombre
+ */
+public String getNombre() {
+	return nombre;
+}
+/**
+ * @param nombre the nombre to set
+ */
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+/**
+ * @return the peso
+ */
+public double getPeso() {
+	return peso;
+}
+/**
+ * @param peso the peso to set
+ */
+public void setPeso(double peso) {
+	this.peso = peso;
+}
+
+
+}
