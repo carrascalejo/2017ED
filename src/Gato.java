13c29d4e4deaf7030e395b0a0ccb579162155e83
david <david@david.com>
2017-02-06 17:01:28 +0100

    Version regreso al futuri o

--- src/Gato.java
+++ src/Gato.java
@@ -1,4 +1,19 @@
 
 public class Gato {
+	int num_vid;
+
+	/**
+	 * @return the num_vid
+	 */
+	public int getNum_vid() {
+		return num_vid;
+	}
+
+	/**
+	 * @param num_vid the num_vid to set
+	 */
+	public void setNum_vid(int num_vid) {
+		this.num_vid = num_vid;
+	}
 
 }
